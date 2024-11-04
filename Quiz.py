2b1ef54a10a3d63d2abd29a9b361b02c96d5d3db
Aeno19 <tempest1906@gmail.com>
2024-11-04 12:45:15 +0300

    Update

---
+++ Quiz.py
@@ -0,0 +1,46 @@
+print("Quiz.")
+
+#question1
+print("What is the fourth planet in the Solar System?")
+ans = input("Answer: ").lower()
+if ans == 'mars':
+   print("Right! +1 point!")
+   score += 1
+else:
+   print("Wrong! :(")
+
+#question2
+print("What is the capital city of Japan?")
+ans = input("Answer: ").lower()
+if ans == 'tokyo':
+   print("Right! +1 point!")
+   score += 1
+else:
+   print("Wrong! :(")
+
+#question3
+print("Who was the first president of the US?")
+ans = input("Answer: ").lower()
+if ans == 'george washington':
+   print("Right! +1 point!")
+   score += 1
+else:
+   print("Wrong! :(")
+
+#question4
+print("What is the formula of the water?")
+ans = input("Answer: ").lower()
+if ans == 'h2o':
+   print("Right! +1 point!")
+   score += 1
+else:
+   print("Wrong! :(")
+
+#question5
+print("What is the square root of 64?")
+ans = input("Answer: ").lower()
+if ans == '8':
+   print("Right! +1 point!")
+   score += 1
+else:
+   print("Wrong! :(")
